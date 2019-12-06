8bbfa94210ce5d3d3ba603f71acc6656ad253727
Valoodian <49910470+Valoodian@users.noreply.github.com>
2019-12-06 19:09:30 +0000

    aula 26/11/2019

--- RMIServer/src/PlaceManager.java
+++ RMIServer/src/PlaceManager.java
@@ -4,16 +4,24 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 public class PlaceManager extends UnicastRemoteObject implements PlacesListInterface {
     private static final long serialVersionUID = 1L;
 
     private static ArrayList<Place> array = new ArrayList<Place>();
+    public static String[] args;
+    public static List listA = new ArrayList();
 
-    public PlaceManager(int port) throws RemoteException, InterruptedException {
-        UUID uuid = UUID.randomUUID();
+    public <args> PlaceManager(int port) throws RemoteException, InterruptedException {
 
+        String suuid = UUID.randomUUID().toString();
+        /*Random rand = new Random();
+        int rand_int = rand.nextInt(10);
+*/
+/*
         Thread u;
         u = new Thread() {
             public void run() {
@@ -32,12 +40,13 @@ public class PlaceManager extends UnicastRemoteObject implements PlacesListInter
                 }
             }
         };
-        u.start();
-        Thread.sleep(10000);
+        u.start();*/
 
+/*
         Thread t;
         t = new Thread() {
             public void run() {
+
             while (true) {
                 Multicast mr = new Multicast();
                 String response = mr.receive();
@@ -46,8 +55,75 @@ public class PlaceManager extends UnicastRemoteObject implements PlacesListInter
             }
         };
         t.start();
+*/
 
-    }
+
+
+
+
+        //heartbeat
+
+        Thread i = new Thread(() -> {
+            while (true) {
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                /* System.out.println("buum" + "_____:" + suuid);*/
+                    Multicast mu = new Multicast();
+                    mu.send("hearthbeat" + "," + suuid);
+
+            }
+        });
+        i.start();
+
+        Thread a = new Thread(() -> {
+
+                while (true) {
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    int tamanholista = listA.size();
+                    int index_lista = tamanholista - 1;
+                    Multicast mr = new Multicast();
+                    String response = mr.receive();
+
+                    String[] output = response.split(",");
+                    if(output[0].equals("hearthbeat")){
+                        if (output[1].equals(suuid)){
+                           // System.out.println("o id é igual ao meu, logo nao vou adicionar À minha lista");
+                        }else{
+                            tamanholista = tamanholista-1;
+                            for(;index_lista < tamanholista;index_lista++) {
+                                if(listA.contains(suuid)){
+                                  listA.add(suuid);
+                              System.out.println(tamanholista);
+                                    //System.out.println("adicionei À lista o id" + suuid);
+
+                        }else{
+                                //nao faz nada
+                                }}}}
+                    else {
+                           // System.out.println("esta mensagem não é um hearthbeat");
+                    }
+                }
+            });
+        a.start();}
+/*
+        String phone = "012-3456789";
+        String[] output = phone.split("-");
+        System.out.println(output[0]);
+        System.out.println(output[1]);*/
+
+
+
+
+
+
 
     @Override
     public void addPlace(Place p) {
